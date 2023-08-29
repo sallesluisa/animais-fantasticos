ae5ead91912590b5bb3f558f034edb5c91d54ba7
Luísa Salles <luisacarvalhosalles@gmail.com>
2023-08-29 17:20:10 -0300

    eslint e babel

---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,12 @@
+export default function initFetchBitcoin() {
+  fetch('https://www.blockchain.com/pt/ticker')
+  .then((response) => response.json())
+  .then(bitcoin => {
+    const btcPreco = document.querySelector('.btc-preco');
+    btcPreco.innerText = (5000 / bitcoin.BRL.buy).toFixed(4);
+    console.log();
+  }).catch(erro => {
+    console.log(Error(erro));
+  }) 
+
+}
