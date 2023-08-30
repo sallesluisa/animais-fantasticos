bb498a0c3b60ba05f8cba3029e743aa40923d8f4
Luísa Salles <luisacarvalhosalles@gmail.com>
2023-08-30 19:47:38 -0300

    :recycle: refactor: Fetch Bitcoin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,11 +1,11 @@
-export default function initFetchBitcoin() {
-  fetch('https://www.blockchain.com/pt/ticker')
-  .then((response) => response.json())
-  .then(bitcoin => {
-    const btcPreco = document.querySelector('.btc-preco');
-    btcPreco.innerText = (5000 / bitcoin.BRL.buy).toFixed(4);
-    console.log();
-  }).catch(erro => {
-    console.log(Error(erro));
-  }) 
+export default function initFetchBitcoin(url, target) {
+  fetch(url)
+    .then((response) => response.json())
+    .then(bitcoin => {
+      const btcPreco = document.querySelector(target);
+      btcPreco.innerText = (5000 / bitcoin.BRL.buy).toFixed(4);
+      console.log();
+    }).catch(erro => {
+      console.log(Error(erro));
+    }) 
 }
